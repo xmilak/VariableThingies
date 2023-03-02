97bbc8b2964a50030d113ecccc71013b932e3b95
Neven Milakovic <xxmilak@gmail.com>
2023-03-02 14:58:03 -0500

    variable project

---
+++ project1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring Variable
+            string myFirstName = "Neven";
+            string myLastName = "Milakovic";
+            string myDogsName = "Apollo";
+
+            //Initialize Variable
+            int myDogWeight = 69;
+
+            Console.WriteLine($"My name is {myFirstName} {myLastName} and this is my doggo {myDogsName}. He's quiet heavy, {myDogWeight} lbs to be more precise");
+        }
+    }
+}
